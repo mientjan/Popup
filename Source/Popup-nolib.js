3764b5aa2c8e6c53df46b101bf8e11474bc2308a
Mient-jan Stelling <mientjan.stelling@gmail.com>
2011-12-11 18:19:28 +0100

    a lot of bug fixes

--- Source/Popup-nolib.js
+++ Source/Popup-nolib.js
@@ -30,8 +30,6 @@ var Popup = function(url, options){
 		}		
 	}
 
-	
-	this.setOptions(options);
 	this.callback = [];
 
 	this.reference = 'n'+Popup._referenceCount;
@@ -90,16 +88,17 @@ Popup.prototype.open = function(){
 		
 	var params = [];
 	for (var key in this.options){
-		if (this.options.hasOwnProperty(key)){
-			if(key=='x' || key=='y'){
-				return;
-			}
+		if(key!='x' && key!='y'){
+			if (this.options.hasOwnProperty(key)){
 
-			if( key == 'width' || key == 'height' ){
-				value = parseInt(value) + 'px';	
+				var value = this.options[key];
+
+				if( key == 'width' || key == 'height' ){
+					value = parseInt(value) + 'px';	
+				}
+
+				params.push( key + '=' + value );
 			}
-			
-			params.push( key + '=' + value );
 		}
 	}
 
@@ -175,8 +174,8 @@ Popup.prototype.callbackIntervalFunction = function(scope){
 		try { // try catch inplace because of popup being able to go crossdomain.
 			if( scope.window.Popup._callback.length > 0 ){
 				
-				Array.each(scope.callback, function(fn, i){
-					Array.each(this.window.Popup._callback, function(data, i){
+				scope.callback.forEach(function(fn, i){
+					this.window.Popup._callback.forEach( function(data, i){
 						fn.call(fn, data);
 					}, scope );
 				}, scope );
@@ -201,6 +200,7 @@ Popup._closeInterval = null;
 
 Popup.fireCallback = function( data ){
 	this._callback.push(data);
+
 }
 
 Popup.close = function(){
@@ -226,4 +226,76 @@ Popup.close = function(){
 		
 	}
 	
+}
+
+/*
+if( !Array.prototype.forEach )
+{
+	Array.prototype.forEach = function(fn,scope){
+		for(var i = 0, len = this.length;i<len;i++){
+			fn.call(scope, this[i], i, this);
+		}
+	}
+}*/
+
+// using this example from mozilla instead.
+
+
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.com/#x15.4.4.18
+if ( !Array.prototype.forEach ) {
+
+  Array.prototype.forEach = function( callback, thisArg ) {
+
+    var T, k;
+
+    if ( this == null ) {
+      throw new TypeError( " this is null or not defined" );
+    }
+
+    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if ( {}.toString.call(callback) != "[object Function]" ) {
+      throw new TypeError( callback + " is not a function" );
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if ( thisArg ) {
+      T = thisArg;
+    }
+
+    // 6. Let k be 0
+    k = 0;
+
+    // 7. Repeat, while k < len
+    while( k < len ) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if ( k in O ) {
+
+        // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+        kValue = O[ k ];
+
+        // ii. Call the Call internal method of callback with T as the this value and
+        // argument list containing kValue, k, and O.
+        callback.call( T, kValue, k, O );
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined
+  };
 }
